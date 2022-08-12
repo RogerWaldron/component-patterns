eed5468df272698f721a9dbe992993ec98756be3
Roger Waldron <roger.waldron@gmail.com>
2022-08-12 19:32:41 +0200

    feat: :heavy_plus_sign: Added TailwindCSS, HeadlessUI and HeroIcons
    Added TailwindCSS and related libs for styling project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import Navbar from '../components/navbar'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="z-0 max-w-screen-xl mx-auto antialiased bg-white text-slate-500 dark:text-slate-400 dark:bg-slate-900">
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
