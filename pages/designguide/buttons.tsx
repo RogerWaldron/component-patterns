1c35ae82a4c68ca0c4a1974e4cfeb7e45b0bacb1
Roger Waldron <roger.waldron@gmail.com>
2022-08-12 22:18:58 +0200

    style: :art: Updated imports and layout of components

--- pages/designguide/buttons.tsx
+++ pages/designguide/buttons.tsx
@@ -1,68 +1,151 @@
-import React from 'react'
+import React from "react";
 
-import { SimpleButton, LinkButton, PlayfulButton, BrutalButton, ElegantButton, PrimaryButton } from '../../components/button'
+import BrutalButton from "../../components/button/brutal";
+import ElegantButton from "../../components/button/elegant";
+import LinkButton from "../../components/button/link";
+import PlayfulButton from "../../components/button/playful";
+import PrimaryButton from "../../components/button/primary";
+import SimpleButton from "../../components/button/simple";
 
-type Props = {}
+type Props = {};
 
 function MyButtons({}: Props) {
   return (
-    <div>
-        <div className="flex py-6 bg-gray-200 justify-evenly">
-        <SimpleButton>Simple</SimpleButton>
-        <SimpleButton disabled>Disabled</SimpleButton>
-        <LinkButton>Link</LinkButton>
-        <PlayfulButton>Playful</PlayfulButton>
-        <BrutalButton>Brutal</BrutalButton>
-        <ElegantButton onClick={() => alert("hello")} aria-label="test">Elegant</ElegantButton>
-        <PrimaryButton onClick={() => alert('Wooot')}>Primary</PrimaryButton>
+    <div className="flex flex-col gap-8">
+      <div className="bg-gray-200">
+        <h3 className="flex justify-center py-2 text-2xl text-gray-500 bg-white">
+          bg-gray-200
+        </h3>
+        <div className="flex py-6 justify-evenly">
+          <SimpleButton>Simple</SimpleButton>
+          <SimpleButton disabled>Disabled</SimpleButton>
+          <LinkButton>Link</LinkButton>
+          <PlayfulButton>Playful</PlayfulButton>
+          <BrutalButton>Brutal</BrutalButton>
+          <ElegantButton onClick={() => alert("hello")} aria-label="test">
+            Elegant
+          </ElegantButton>
+          <PrimaryButton onClick={() => alert("Wooot")}>Primary</PrimaryButton>
+        </div>
+        <div className="flex py-6 justify-evenly">
+          <SimpleButton disabled>Simple</SimpleButton>
+          <SimpleButton disabled>Disabled</SimpleButton>
+          <LinkButton disabled>Link</LinkButton>
+          <PlayfulButton disabled>Playful</PlayfulButton>
+          <BrutalButton disabled>Brutal</BrutalButton>
+          <ElegantButton
+            disabled
+            onClick={() => alert("hello")}
+            aria-label="test"
+          >
+            Elegant
+          </ElegantButton>
+          <PrimaryButton disabled onClick={() => alert("Wooot")}>
+            Primary
+          </PrimaryButton>
+        </div>
       </div>
-      <div className="flex py-6 bg-gray-600 justify-evenly">
-        <SimpleButton disabled >Simple</SimpleButton>
-        <SimpleButton disabled>Disabled</SimpleButton>
-        <LinkButton disabled>Link</LinkButton>
-        <PlayfulButton disabled>Playful</PlayfulButton>
-        <BrutalButton  disabled>Brutal</BrutalButton>
-        <ElegantButton disabled onClick={() => alert("hello")} aria-label="test">Elegant</ElegantButton>
-        <PrimaryButton disabled onClick={() => alert('Wooot')}>Primary</PrimaryButton>
+      <div className="bg-gray-400">
+        <h3 className="flex justify-center py-2 text-2xl text-gray-500 bg-white">
+          bg-gray-400
+        </h3>
+        <div className="flex py-6 justify-evenly">
+          <SimpleButton>Simple</SimpleButton>
+          <SimpleButton disabled>Disabled</SimpleButton>
+          <LinkButton>Link</LinkButton>
+          <PlayfulButton>Playful</PlayfulButton>
+          <BrutalButton>Brutal</BrutalButton>
+          <ElegantButton onClick={() => alert("hello")} aria-label="test">
+            Elegant
+          </ElegantButton>
+          <PrimaryButton onClick={() => alert("Wooot")}>Primary</PrimaryButton>
+        </div>
+        <div className="flex py-6 justify-evenly">
+          <SimpleButton disabled>Simple</SimpleButton>
+          <SimpleButton disabled>Disabled</SimpleButton>
+          <LinkButton disabled>Link</LinkButton>
+          <PlayfulButton disabled>Playful</PlayfulButton>
+          <BrutalButton disabled>Brutal</BrutalButton>
+          <ElegantButton
+            disabled
+            onClick={() => alert("hello")}
+            aria-label="test"
+          >
+            Elegant
+          </ElegantButton>
+          <PrimaryButton disabled onClick={() => alert("Wooot")}>
+            Primary
+          </PrimaryButton>
+        </div>
       </div>
-      <div className="flex py-6 bg-gray-400 justify-evenly">
-        <SimpleButton>Simple</SimpleButton>
-        <SimpleButton disabled>Disabled</SimpleButton>
-        <LinkButton>Link</LinkButton>
-        <PlayfulButton>Playful</PlayfulButton>
-        <BrutalButton>Brutal</BrutalButton>
-        <ElegantButton onClick={() => alert("hello")} aria-label="test">Elegant</ElegantButton>
-        <PrimaryButton onClick={() => alert('Wooot')}>Primary</PrimaryButton>
+      <div className="bg-gray-600">
+        <h3 className="flex justify-center py-2 text-2xl text-gray-500 bg-white">
+          bg-gray-600
+        </h3>
+        <div className="flex py-6 justify-evenly">
+          <SimpleButton>Simple</SimpleButton>
+          <SimpleButton disabled>Disabled</SimpleButton>
+          <LinkButton>Link</LinkButton>
+          <PlayfulButton>Playful</PlayfulButton>
+          <BrutalButton>Brutal</BrutalButton>
+          <ElegantButton onClick={() => alert("hello")} aria-label="test">
+            Elegant
+          </ElegantButton>
+          <PrimaryButton onClick={() => alert("Wooot")}>Primary</PrimaryButton>
+        </div>
+        <div className="flex py-6 justify-evenly">
+          <SimpleButton disabled>Simple</SimpleButton>
+          <SimpleButton disabled>Disabled</SimpleButton>
+          <LinkButton disabled>Link</LinkButton>
+          <PlayfulButton disabled>Playful</PlayfulButton>
+          <BrutalButton disabled>Brutal</BrutalButton>
+          <ElegantButton
+            disabled
+            onClick={() => alert("hello")}
+            aria-label="test"
+          >
+            Elegant
+          </ElegantButton>
+          <PrimaryButton disabled onClick={() => alert("Wooot")}>
+            Primary
+          </PrimaryButton>
+        </div>
       </div>
-      <div className="flex py-6 bg-gray-600 justify-evenly">
-        <SimpleButton disabled >Simple</SimpleButton>
-        <SimpleButton disabled>Disabled</SimpleButton>
-        <LinkButton disabled>Link</LinkButton>
-        <PlayfulButton disabled>Playful</PlayfulButton>
-        <BrutalButton  disabled>Brutal</BrutalButton>
-        <ElegantButton disabled onClick={() => alert("hello")} aria-label="test">Elegant</ElegantButton>
-        <PrimaryButton disabled onClick={() => alert('Wooot')}>Primary</PrimaryButton>
-      </div>
-      <div className="flex py-6 bg-gray-800 justify-evenly">
-        <SimpleButton >Simple</SimpleButton>
-        <SimpleButton disabled>Disabled</SimpleButton>
-        <LinkButton>Link</LinkButton>        
-        <PlayfulButton>Playful</PlayfulButton>
-        <BrutalButton>Brutal</BrutalButton>
-        <ElegantButton onClick={() => alert("hello")} aria-label="test">Elegant</ElegantButton>
-        <PrimaryButton onClick={() => alert('Wooot')}>Primary</PrimaryButton>
-      </div>
-      <div className="flex py-6 bg-gray-600 justify-evenly">
-        <SimpleButton disabled>Simple</SimpleButton>
-        <SimpleButton disabled>Disabled</SimpleButton>
-        <LinkButton disabled>Link</LinkButton>
-        <PlayfulButton disabled>Playful</PlayfulButton>
-        <BrutalButton  disabled>Brutal</BrutalButton>
-        <ElegantButton disabled onClick={() => alert("hello")} aria-label="test">Elegant</ElegantButton>
-        <PrimaryButton disabled onClick={() => alert('Wooot')}>Primary</PrimaryButton>
+      <div className="bg-gray-800">
+        <h3 className="flex justify-center py-2 text-2xl text-gray-500 bg-white">
+          bg-gray-800
+        </h3>
+        <div className="flex py-6 justify-evenly">
+          <SimpleButton>Simple</SimpleButton>
+          <SimpleButton disabled>Disabled</SimpleButton>
+          <LinkButton>Link</LinkButton>
+          <PlayfulButton>Playful</PlayfulButton>
+          <BrutalButton>Brutal</BrutalButton>
+          <ElegantButton onClick={() => alert("hello")} aria-label="test">
+            Elegant
+          </ElegantButton>
+          <PrimaryButton onClick={() => alert("Wooot")}>Primary</PrimaryButton>
+        </div>
+        <div className="flex py-6 justify-evenly">
+          <SimpleButton disabled>Simple</SimpleButton>
+          <SimpleButton disabled>Disabled</SimpleButton>
+          <LinkButton disabled>Link</LinkButton>
+          <PlayfulButton disabled>Playful</PlayfulButton>
+          <BrutalButton disabled>Brutal</BrutalButton>
+          <ElegantButton
+            disabled
+            onClick={() => alert("hello")}
+            aria-label="test"
+          >
+            Elegant
+          </ElegantButton>
+          <PrimaryButton disabled onClick={() => alert("Wooot")}>
+            Primary
+          </PrimaryButton>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default MyButtons
+export default MyButtons;
