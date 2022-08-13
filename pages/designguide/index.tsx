3f3ef415c1ad9795a23a6b314c25d5b9270bcd2c
Roger Waldron <roger.waldron@gmail.com>
2022-08-13 19:58:05 +0200

    docs: :memo: Improved description of page

--- pages/designguide/index.tsx
+++ pages/designguide/index.tsx
@@ -6,8 +6,10 @@ type Props = {};
 function DesignGuide({}: Props) {
   return (
     <div className="container mx-auto text-neutral-700">
-      <h2 className="flex justify-center py-8 text-2xl">Design Guide</h2>
-      <p>Playground for testing ideas on how to construct these components</p>
+      <h2 className="flex justify-center py-8 text-2xl">
+        Design Pattern Guide
+      </h2>
+      <p>Links to showcase pages for these components</p>
       <ul className="flex flex-col gap-4 mt-8">
         <li className="">
           <Link href="../designguide/variant">Variant Buttons</Link>
